b890a0089e34c93f41f79f8490b21cad3a5dbc11
xuan940109 <dora.lhliang@gmail.com>
2024-03-13 11:56:21 +0800

    week04

---
+++ week04/week04-4.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a=2;
+    while(a>0)
+    {
+        printf("a¬O%d\n",a);
+        a--;
+    }
+
+    for (int b=3; b>0; b--){
+        printf("b¬O%d\n",b);
+    }
+}
---
+++ week04/week04-5.cpp
@@ -0,0 +1,14 @@
+//week04-5.cpp
+#include <stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	
+	int ans = 1;
+	for(int i=1; i<=a; i++){
+		ans *= i;
+	}
+	
+	printf("%d",ans);
+}
