1c96d4bf79265a25a0f6f49cdb3a222ff34e726c
xuan940109 <dora.lhliang@gmail.com>
2024-04-17 11:03:18 +0800

    week09

---
+++ week09/week09-2.cpp
@@ -0,0 +1,13 @@
+///week09-2.cpp 要了解字串與陣列的關係
+#include <stdio.h>
+int main()
+{
+    char line[80];
+    printf("請輸入一行英文,有空格沒關係:\n");
+    gets(line);
+    printf("這行字是:%s\n",line);
+
+    printf("請再輸入一行英文:\n");
+    scanf("%s",line);
+    printf("這行字是:%s\n",line);
+}
