19709165cbcb53bd256e1de5570b569bdbc93f01
xuan940109 <dora.lhliang@gmail.com>
2024-04-24 11:32:39 +0800

    week10

---
+++ week10/week10-4.cpp
@@ -0,0 +1,18 @@
+///week10-4.cpp 要看變數範圍 variable scope
+#include <stdio.h>
+int z=100;
+void myFunc(){
+    int x=20;
+    printf("myFunc()裡的 x: %d z: %d\n",x,z);
+    printf("在離開時，順手把x和z都設定 0\n");
+    x=0;
+    z=0;
+}
+
+int main()
+{
+    int x=10;
+    printf("main() 裡的 x: %d z: %d\n",x,z);
+    myFunc();
+    printf("main() 裡的 x: %d z: %d\n",x,z);
+}
