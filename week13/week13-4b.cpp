4a5baf7d4ee179e7320fd06d2bc5a9351405457c
xuan940109 <dora.lhliang@gmail.com>
2024-05-15 12:02:17 +0800

    week13

---
+++ week13/week13-4b.cpp
@@ -0,0 +1,13 @@
+///week13-4b.cpp
+#include <stdio.h>
+int gcd(int a, int b){
+	if(a==0)return b;
+	if(b==0)return a;
+	return gcd(b,a%b);
+}
+int main()
+{
+  int a,b;
+  scanf("%d %d",&a, &b);
+  printf("%d\n",gcd(a,b) );
+}
