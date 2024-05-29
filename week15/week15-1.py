a45e0572daec7961acbfeb548669da3885d4a284
xuan940109 <dora.lhliang@gmail.com>
2024-05-29 10:03:36 +0800

    week15

---
+++ week15/week15-1.py
@@ -0,0 +1,8 @@
+#week15-1.py ¼g¨ì¤@¥bªºpythonª©
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        N=len(s)
+        for i in range(N):
+            print(s[i],t[i])
+
+        return 0
